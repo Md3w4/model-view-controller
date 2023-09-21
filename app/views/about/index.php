6d08e71379e3063e2582fa003024b425d529b8c6
Md3w4 <mahadewamm109@gmail.com>
2023-09-21 13:06:01 +0700

    Last Up

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,6 @@
-<div class="container">
+<div class="container d-flex flex-column justify-content-center align-items-center">
     <h1>About Me</h1>
-    <img src="<?= BASEURL; ?>/img/profil.jpg" alt="profil" width="200" class="rounded-circle shadow">
+    <img src="<?= BASEURL; ?>/img/profil.jpg" alt="profil" width="200" class="rounded-circle shadow mb-2">
     <p>Halo, nama Saya <?= $data['nama']; ?>, umur Saya <?= $data['umur']; ?> tahun, Saya adalah seorang
         <?= $data['pekerjaan'] ?>.</p>
 </div>
